44b8dcc0af1fe40cbb126d8e4b18af05c8f05b6e
Prasetyo Adi Pratama Nugroho <prazetyo111@gmail.com>
2022-11-17 03:39:17 +0700

    Change Dashboard text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <main>
     <div class="container-fluid px-4">
-        <h1 class="mt-4">Dashboard</h1>
+        <h1 class="mt-4">Sunnyrent Cost</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">Dashboard</li>
         </ol>
